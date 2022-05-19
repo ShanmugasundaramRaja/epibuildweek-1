ed3d257878c56f6565aa20802b60ea83236137fc
Harika_Kommuri <harikakommuri35@gmail.com>
2022-05-19 16:30:11 +0200

    Updated the JS to display questions

--- questionPage.js
+++ questionPage.js
@@ -1,40 +1,38 @@
-let pos = 0;
+let position = 0;
 
 let questions = [
-    { "question": "A variable in JS is declared as:", "options": ["let", "Int", "String", "New"], "answer": "let" },
-    { "question": "Which one is not a Primitive DataType?", "options": ["char", "defined", "boolean", "number"], "answer": "char" },
-    { "question": "How to call function named foo?", "options": ["foo()();", "foo();", "function foo()", "foo;"], "answer": "push()" },
-    { "question": "Role of JS in Web development?", "options": ["Style", "None", "Structure", "Behaviour"], "answer": "Behaviour" }
-];
+    { "question": "A variable in JS is declared as:", "options": ["let", "Int", "String", "New"] },
+    { "question": "Which one is not a Primitive DataType?", "options": ["char", "defined", "boolean", "number"] },
+    { "question": "How to call function named foo?", "options": ["foo()();", "foo();", "function foo()", "foo;"] },
+    { "question": "Role of JS in Web development?", "options": ["Style", "None", "Structure", "Behaviour"] }
+]
 
 const displayQuestions = function () {
-    let wrap = document.querySelector(".questions");
-    wrap.style.display = "visible"
-    const question = questions[pos];
-    wrap.innerHTML = `
-        <div class="questions${pos}">
-            <p>${question.question}</p>
+    let questionNode = document.querySelector(".questions");
+    questionNode.style.display = "visible"
+    let questionNum = questions[position];
+    questionNode.innerHTML = `
+        <div class="questions${position}">
+            <p>${questionNum.question}</p>
             <div id="answer-buttons" class="btn-grid">
-                <input type="button" class="button1" value="${question.options[0]}" />
-                <input type="button" class="button2" value="${question.options[1]}" />
-                <input type="button" class="button3" value="${question.options[2]}" />
-                <input type="button" class="button4" value="${question.options[3]}" />
+                <input type="button" class="button1" value="${questionNum.options[0]}" />
+                <input type="button" class="button2" value="${questionNum.options[1]}" />
+                <input type="button" class="button3" value="${questionNum.options[2]}" />
+                <input type="button" class="button4" value="${questionNum.options[3]}" />
             </div>
-        </div>`;
+        </div>`
 }
 
-function next() {
-    let nextQuestion = document.getElementById("nextbutton")
-    nextQuestion.addEventListener("click", function (e) {
-        pos++
+
+let nextQuestion = document.getElementById("nextbutton")
+nextQuestion.addEventListener("click", function (e) {
+    if (position >= questions.length - 1) {
+        window.location = "./buildproject.html"
+    } else {
+        position++
         displayQuestions()
-    })
-}
-
-next()
-
-
-
+    }
+})
 
 window.onload = function () {
     displayQuestions()
